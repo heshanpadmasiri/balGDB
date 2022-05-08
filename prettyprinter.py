e5682e536a4b1687880c8f5df441b01d6f3a0d88
Heshan Padmasiri <heshanp@wso2.com>
2022-05-08 22:09:33 +0530

    add float support

--- prettyprinter.py
+++ prettyprinter.py
@@ -3,16 +3,18 @@ import gdb
 
 HEAP_ALIGNMENT = 8
 TAG_SHIFT = 56
+
 POINTER_MASK = (1 << TAG_SHIFT) - 1
 ALIGN_MASK = ~(HEAP_ALIGNMENT - 1)
 IMMEDIATE_FLAG = (0x20) << TAG_SHIFT
+TAG_MASK = 0xFF
+UT_MASK = 0x1F
 
-class VarPrinter:
-    def __init__(self, val):
-        self.val = val
+TAG_NIL = 0x00
+TAG_BOOLEAN = 0x01
+TAG_INT = 0x07
+TAG_FLOAT = 0x08
 
-    def to_string(self):
-        return str(int(self.val))
 
 class TaggedPrinter:
     def __init__(self, val):
@@ -23,9 +25,21 @@ class TaggedPrinter:
         ptr_body = ptr_val & POINTER_MASK
         if ptr_val & IMMEDIATE_FLAG != 0:
             return str(ptr_body)
+        tag = self.get_tag() & UT_MASK
         new_ptr = (ptr_body & ALIGN_MASK)
-        new_val = gdb.Value(new_ptr).cast(self.val.type).dereference()
-        return str(int(new_val))
+        if tag == TAG_INT:
+            long_ty = gdb.lookup_type("long")
+            new_val = gdb.Value(new_ptr).cast(long_ty.pointer()).dereference()
+            return str(int(new_val))
+        if tag == TAG_FLOAT:
+            double_ty = gdb.lookup_type("double")
+            new_val = gdb.Value(new_ptr).cast(double_ty.pointer()).dereference()
+            return str(float(new_val))
+        raise Exception("Unimplemented tag")
+
+    def get_tag(self):
+        pointer = int(self.val)
+        return (pointer >> TAG_SHIFT) & TAG_MASK
 
     def bits(self):
         return format(int(self.val), '064b');
@@ -34,7 +48,6 @@ def build_pretty_printer():
     pp = gdb.printing.RegexpCollectionPrettyPrinter(
         "bal_pp")
     pp.add_printer('TaggedPtr', '^TaggedPtr$', TaggedPrinter)
-    pp.add_printer('var', '^var$', VarPrinter)
     return pp
 
 gdb.printing.register_pretty_printer(
